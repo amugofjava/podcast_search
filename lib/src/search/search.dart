d1443143a918d66cf42336b51ae0a454dfa057d8
Ben Hills <ben.hills@amugofjava.me.uk>
2020-08-12 17:21:11 +0100

    Re-run dartfrmt after upgrading to latest Flutter stable.

--- lib/src/search/search.dart
+++ lib/src/search/search.dart
@@ -120,8 +120,7 @@ class Search {
         for (var entry in entries) {
           var id = entry['id']['attributes']['im:id'];
 
-          final response =
-              await _client.get(FEED_API_ENDPOINT + '/lookup?id=$id');
+          final response = await _client.get(FEED_API_ENDPOINT + '/lookup?id=$id');
 
           final results = json.decode(response.data);
 
@@ -197,9 +196,7 @@ class Search {
   }
 
   String _termParam() {
-    return term != null && term.isNotEmpty
-        ? '?term=' + Uri.encodeComponent(term)
-        : '';
+    return term != null && term.isNotEmpty ? '?term=' + Uri.encodeComponent(term) : '';
   }
 
   String _countryParam() {
@@ -207,9 +204,7 @@ class Search {
   }
 
   String _attributeParam() {
-    return _attribute != null
-        ? '&attribute=' + Uri.encodeComponent(_attribute.attribute)
-        : '';
+    return _attribute != null ? '&attribute=' + Uri.encodeComponent(_attribute.attribute) : '';
   }
 
   String _limitParam() {
